8d1b497b87b82e426bd5337cd262d56cb90629eb
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-08-05 01:58:22 +0900

    Added tailwind and post feature

--- components/PostFeed.js
+++ components/PostFeed.js
@@ -1,47 +1,66 @@
 import Link from 'next/link';
+import ReactMarkdown from 'react-markdown';
+import HeartButton from '../components/HeartButton';
+import AuthCheck from '../components/AuthCheck';
+import { doc, getFirestore } from 'firebase/firestore';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+import { useContext } from 'react';
+import { UserContext } from '../lib/context';
 
-export default function PostFeed({ posts, admin }) {
-  return posts ? posts.map((post) => <PostItem post={post} key={post.slug} admin={admin} />) : null;
+
+export default function PostFeed({ posts, admin, project }) {
+  const { user: currentUser, username } = useContext(UserContext);
+  return posts ? posts.map((post) => <PostItem post_slow={post} key={post.slug} admin={admin} project={project} currentUser={currentUser} />) : null;
 }
 
-function PostItem({ post, admin = false }) {
+function PostItem({ post_slow, admin = false , project, currentUser }) {
+
+  const postRef = doc(getFirestore(), 'users', project.uid, 'projects', project.slug, 'posts', post_slow.slug);
+  const [realtimePost] = useDocumentData(postRef);
+  const post = realtimePost || post_slow;
+
   // Naive method to calc word count and read time
   const wordCount = post?.content.trim().split(/\s+/g).length;
   const minutesToRead = (wordCount / 100 + 1).toFixed(0);
+  const createdAt = typeof post?.createdAt === 'number' ? new Date(post.createdAt) : post.createdAt.toDate();
+
 
   return (
     <div className="card">
-      <Link href={`/${post.username}`}>
-        <a>
-          <strong>By @{post.username}</strong>
-        </a>
-      </Link>
 
-      <Link href={`/${post.username}/${post.slug}`}>
-        <h2>
-          <a>{post.title}</a>
-        </h2>
-      </Link>
+      <span className="text-sm">
+        Written by{' '}
+        <Link href={`/${post.username}/`}>
+          <a className="text-info">@{post.username}</a>
+        </Link>{' '}
+        on {createdAt.toISOString()}
+      </span>
+      <ReactMarkdown>{post?.content}</ReactMarkdown>
 
       <footer>
         <span>
           {wordCount} words. {minutesToRead} min read
         </span>
         <span className="push-left">💗 {post.heartCount || 0} Hearts</span>
+        <AuthCheck
+          fallback={
+            <Link href="/enter">
+              <button>💗 Sign Up</button>
+            </Link>
+          }
+        >
+          <HeartButton projectRef={postRef} />
+        </AuthCheck>
       </footer>
 
       {/* If admin view, show extra controls for user */}
-      {admin && (
-        <>
-          <Link href={`/admin/${post.slug}`}>
-            <h3>
-              <button className="btn-blue">Edit</button>
-            </h3>
-          </Link>
-
-          {post.published ? <p className="text-success">Live</p> : <p className="text-danger">Unpublished</p>}
-        </>
-      )}
+      {currentUser?.uid === post.uid && (
+          <>
+            <Link href={`/admin/${project.slug}/${post.slug}`}>
+              <button className="btn-blue">Edit Post</button>
+            </Link>
+          </>
+        )}
     </div>
   );
 }
--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -4,32 +4,45 @@ import HeartButton from '../../components/HeartButton';
 import AuthCheck from '../../components/AuthCheck';
 import Metatags from '../../components/Metatags';
 import { UserContext } from '../../lib/context';
-import { firestore, getUserWithUsername, projectToJSON } from '../../lib/firebase';
-import { doc, getDocs, getDoc, collectionGroup, query, limit, getFirestore } from 'firebase/firestore';
+import { firestore, getUserWithUsername, projectToJSON, auth } from '../../lib/firebase';
+import { serverTimestamp, doc, getDocs, getDoc, setDoc, collectionGroup, query, limit, getFirestore, collection, where, orderBy } from 'firebase/firestore';
+import { useForm } from 'react-hook-form';
+import ImageUploader from '../../components/ImageUploader';
 
 
 import Link from 'next/link';
 import { useDocumentData } from 'react-firebase-hooks/firestore';
 import { useContext } from 'react';
+import toast from 'react-hot-toast';
+import PostFeed from '../../components/PostFeed';
 
-
+// SSG
 export async function getStaticProps({ params }) {
   const { username, slug } = params;
   const userDoc = await getUserWithUsername(username);
 
   let project;
   let path;
-
+  let posts = null;
+  
   if (userDoc) {
     const projectRef = doc(getFirestore(), userDoc.ref.path, 'projects', slug);
 
     project = projectToJSON(await getDoc(projectRef) );
 
     path = projectRef.path;
+
+    const postsQuery = query(
+      collection(getFirestore(), userDoc.ref.path, 'projects', slug, 'posts'),
+      where('published', '==', true),
+      orderBy('createdAt', 'desc'),
+      limit(5)
+    );
+    posts = (await getDocs(postsQuery)).docs.map(projectToJSON);
   }
 
   return {
-    props: { project, path },
+    props: { project, path, posts },
     revalidate: 100,
   };
 }
@@ -59,34 +72,37 @@ export async function getStaticPaths() {
   };
 }
 
+// shows edit project and add post UI for the owner
 export default function Project(props) {
   const projectRef = doc(getFirestore(), props.path);
   const [realtimeProject] = useDocumentData(projectRef);
-
   const project = realtimeProject || props.project;
 
-  const { user: currentUser } = useContext(UserContext);
+  const { user: currentUser, username } = useContext(UserContext);
 
   return (
     <main className={styles.container}>
       <Metatags title={project.title} description={project.title} />
-      
-      <section>
-        <ProjectContent project={project} />
-      </section>
+      <div className="flex flex-col">
+        
+        <section>
+          <ProjectContent project={project} />
+        </section>
 
-      <div className="card">
-      {currentUser?.uid === project.uid && (
-          <>
-            <Link href={`/admin/${project.slug}`}>
-              <button className="btn-blue">Edit Project</button>
-            </Link>
-            <Link href={`/admin/${project.slug}/update`}>
-              <button className="btn-blue">Add Update</button>
-            </Link>
-          </>
-        )}
+        <div className="">
+          <PostFeed posts={props.posts} project={project}/>
+        </div>
+
+        <div className="card">
+        {currentUser?.uid === project.uid && (
+            <>
+              <p>Add post here</p>
+              <PostForm project={project} username={username}  />
+            </>
+          )}
+        </div>
       </div>
+      
       <aside className="card">
         <p>
           <strong>{project.heartCount || 0} 🤍</strong>
@@ -107,12 +123,89 @@ export default function Project(props) {
             <Link href={`/admin/${project.slug}`}>
               <button className="btn-blue">Edit Project</button>
             </Link>
-            {/* <Link href={`/admin/${project.slug}/update`}>
-              <button className="btn-blue">Add Update</button>
-            </Link> */}
           </>
         )}
       </aside>
     </main>
+  );
+}
+
+function PostForm({ project, username }) {
+  const preview=false; // todo
+
+  const { register, formState: { errors, isValid, isDirty }, handleSubmit, reset, watch } = useForm({ defaultValues: {}, mode: 'onChange' });
+  const submitPost = async ({ content, published }) => {
+    const uid = auth.currentUser.uid;
+    const ref = doc(getFirestore(), 'users', uid, 'projects', project.slug, 'posts', "test"); // todo
+
+    // Tip: give all fields a default value here
+    const data = {
+      slug: "test", // todo
+      uid,
+      username,
+      published,
+      content,
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp(),
+      heartCount: 0,
+    };
+
+    await setDoc(ref, data);
+
+    reset({ content, published });
+
+    toast.success('Post updated successfully!');
+  };
+
+  return (
+    <form onSubmit={handleSubmit(submitPost)}>
+      {preview && (
+        <div className="card">
+          <ReactMarkdown>{watch('content')}</ReactMarkdown>
+        </div>
+      )}
+
+      <div className={preview ? styles.hidden : styles.controls}>
+        <ImageUploader />
+
+        <textarea
+         {...register('content', {
+            maxLength: { value: 20000, message: 'content is too long' },
+            minLength: { value: 10, message: 'content is too short' },
+            required: { value: true, message: 'content is required' },
+          })}>  
+        </textarea>
+
+        {errors.content && <p className="text-danger">{errors.content.message}</p>}
+
+        <fieldset>
+          <input className={styles.checkbox} type="checkbox" {...register('published', {})}/>
+          <label>Published</label>
+        </fieldset>
+
+        <button type="submit" className="btn-green" disabled={!isDirty || !isValid}>
+          Save Changes
+        </button>
+      </div>
+    </form>
+  );
+}
+
+function DeletePostButton({ postRef }) {
+  const router = useRouter();
+
+  const deletePost = async () => {
+    const doIt = confirm('are you sure!');
+    if (doIt) {
+      await deleteDoc(postRef);
+      router.push('/admin');
+      toast('post annihilated ', { icon: '🗑️' });
+    }
+  };
+
+  return (
+    <button className="btn-red" onClick={deletePost}>
+      Delete
+    </button>
   );
 }
