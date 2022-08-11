9064de03d532f14e30adc0bcce82e48abf8d0d7e
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-08-11 16:19:24 +0900

    japanese username rejection and reroute to / after validation of username

--- pages/enter.js
+++ pages/enter.js
@@ -3,12 +3,18 @@ import { doc, writeBatch, getDoc, getFirestore } from 'firebase/firestore';
 import { signInWithPopup, signInAnonymously, signOut } from 'firebase/auth';
 import { UserContext } from '../lib/context';
 import Metatags from '../components/Metatags';
+import { useRouter } from 'next/router';
+
 
 import { useEffect, useState, useCallback, useContext } from 'react';
 import debounce from 'lodash.debounce';
 
 export default function Enter(props) {
   const { user, username } = useContext(UserContext);
+  const router = useRouter();
+  useEffect(() => {
+    username && router.push('/')
+  }, [username]);
 
   // 1. user signed out <SignInButton />
   // 2. user signed in, but missing username <UsernameForm />
@@ -51,7 +57,8 @@ function UsernameForm() {
   const [loading, setLoading] = useState(false);
 
   const { user, username } = useContext(UserContext);
-
+  const router = useRouter();
+  
   const onSubmit = async (e) => {
     e.preventDefault();
 
@@ -65,21 +72,20 @@ function UsernameForm() {
     batch.set(usernameDoc, { uid: user.uid });
 
     await batch.commit();
+    router.push('/');
   };
 
   const onChange = (e) => {
     // Force form value typed in form to match correct format
     const val = e.target.value.toLowerCase();
-    const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+    // const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+    const re = /^[0-9a-zA-Z]*$/;
 
-    // Only set form value if length is < 3 OR it passes regex
-    if (val.length < 3) {
+    if (val.length==0) {
       setFormValue(val);
       setLoading(false);
       setIsValid(false);
-    }
-
-    if (re.test(val)) {
+    }else if (re.test(val)) {
       setFormValue(val);
       setLoading(true);
       setIsValid(false);
@@ -96,11 +102,13 @@ function UsernameForm() {
   // useCallback is required for debounce to work
   const checkUsername = useCallback(
     debounce(async (username) => {
-      if (username.length >= 3) {
+      if (username.length >= 1) {
         const ref = doc(getFirestore(), 'usernames', username);
         const snap = await getDoc(ref);
-        console.log('Firestore read executed!', snap.exists());
         setIsValid(!snap.exists());
+        if (username == 'admin' || username == 'api' || username == '404' || username == 'enter' || username == 'posts'){
+          setIsValid(false);
+        }
         setLoading(false);
       }
     }, 500),
@@ -110,7 +118,7 @@ function UsernameForm() {
   return (
     !username && (
       <section>
-        <h3>Choose Username</h3>
+        <h3>Choose Username (半角英数字)</h3>
         <form onSubmit={onSubmit}>
           <input name="username" placeholder="myname" value={formValue} onChange={onChange} />
           <UsernameMessage username={formValue} isValid={isValid} loading={loading} />
