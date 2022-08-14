e9fea77aec11bc0112fb71c9a47e95b69c355f33
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-08-14 22:39:37 +0900

    Added create new project button for not signed up users

--- components/Navbar.js
+++ components/Navbar.js
@@ -78,13 +78,46 @@ export default function Navbar() {
         )}
 
         {/* user is not signed OR has not created username */}
+
         {!username && (
-          <div className="navbar-end ">
-            <Link href="/enter">
-              <button className="btn btn-primary md:btn-wide text-white">Log in</button>
-            </Link>
-          </div>
+          <div className="navbar-end md:hidden">
+            <div className="dropdown dropdown-end">
+              <label tabIndex="0" className="btn btn-ghost btn-circle">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+              </label>
+              <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                <li>
+                  <Link href="/enter">
+                    <button className="bg-primary text-white">create new project</button>
+                  </Link>
+                </li>
+                <li className="">
+                  <Link href="/enter">
+                    <button className="">Log in</button>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>)}
+
+
+        {!username && (
+          <div className="md:flex-none md:flex hidden">
+          <ul className="menu menu-horizontal p-0">
+            <li>
+              <Link href="/enter">
+                <button className="btn btn-primary text-white my-auto mx-3">create new project</button>
+              </Link>
+            </li>
+            <li className="">
+              <Link href="/enter">
+                <button className="btn btn-primary text-white">Log in</button>
+              </Link>
+            </li>
+          </ul>
+        </div>
         )}
+
     </nav>
   );
 }
